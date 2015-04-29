b35b2e85fa7eefb39138a9be74b08df8022180c8
Peter Krieg <pkrieg2@gmail.com>
2015-04-29 01:29:11 -0400

    started making select menu, keeping only 1 HTML file, and loading up differnet coderbyte challenges using jquery

--- easy/js/js.js
+++ easy/js/js.js
@@ -1,14 +1,64 @@
 $(function(){
 
-	$('input.input').keydown(function(){
-		$('input.output').val($('input.input').val());
-
+	// This code can stay the same for every challenge.  Whenever enter key is pressed,
+	// The enterKey event is triggered
+	$('input.input').keyup(function(e){
+		if(e.keyCode == 13)
+		{
+			$(this).trigger("enterKey");
+		}
 	});
 
+	// Whenever different option on select menu is clicked, it triggers an event
+	// for differnet function of each challenge
+	$('select#challenges').change(function(){
+		$('select#challenges option:selected').each(function(){
+			 // $(this).parent.trigger($(this).val());
+			alert($(this).val());
+		})
+	})
+
+	// Code to reverse a string
+	$('body.firstReverse input.input').bind("enterKey",function(e){
+		var input = $('input.input').val();
+		var result = "";
+		for(var i=input.length-1; i>=0; i--){
+			result += input[i];
+		}
+		$('body.firstReverse input.output').val(result);
+	});
+
+	// First Factorial Code
+	$('select#challenges').bind("firstFactorial", function(){
+		alert('first factorial ready to go!');
+	})
 	
+
+
+
+	// $('body.firstReverse input.input').bind("enterKey",function(e){
+	// 	var input = $('input.input').val();
+	// 	var result = "";
+	// 	for(var i=input.length-1; i>=0; i--){
+	// 		result += input[i];
+	// 	}
+	// 	$('body.firstReverse input.output').val(result);
+ // });
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 });
+
+
